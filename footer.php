64f10ddd4de69af5d2428d4327875252fe54d9d1
idoten <ian.doten@uvm.edu>
2018-11-02 14:25:32 -0400

    first

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Ian Doten.</p>
+    <p>Code for this site can be found at: <a href="https://gitlab.uvm.edu/Ian.Doten/CS-148-lab4!">https://gitlab.uvm.edu/robert-erickson/Simple-Database-UVM</a></p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,53 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav>
+    <ol>
+        <?php
+        // This sets a class for current page so you can style it differently
+        
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'lab4') {
+            print ' class="activePage" ';
+        }
+        print '><a href="index.php">Home Lab4</a></li>';
+        
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'sitemap') {
+            print ' class="activePage" ';
+        }
+        print '><a href="../sitemap.php">Sitemap</a></li>';
+       
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'sampleQuery') {
+            print ' class="activePage" ';
+        }
+        print '><a href="sampleQuery.php">Sample SELECT</a></li>';
+       
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'sampleQuery2') {
+            print ' class="activePage" ';
+        }
+        print '><a href="sampleQuery2.php">Sample INSERT</a></li>';
+        
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'sampleQuery3') {
+            print ' class="activePage" ';
+        }
+        print '><a href="sampleQuery3.php">Sample SELECT (with ORDER clause)</a></li>';
+        
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'sampleQuery4') {
+            print ' class="activePage" ';
+        }
+        print '><a href="sampleQuery4.php">Sample test (fails)</a></li>';
+        
+        print '<li ';
+        if ($PATH_PARTS['filename'] == 'tables') {
+            print ' class="activePage" ';
+        }
+        print '><a href="tables.php">Tables</a></li>';
+
+        ?>
+    </ol>
+</nav>
+<!-- #################### Ends Main Navigation    ########################## -->
+
