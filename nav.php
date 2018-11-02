f3d9655a2cc841247c18227848fba356c576cf41
idoten <ian.doten@uvm.edu>
2018-11-02 14:56:25 -0400

    set up form

--- nav.php
+++ nav.php
@@ -5,7 +5,7 @@
         // This sets a class for current page so you can style it differently
         
         print '<li ';
-        if ($PATH_PARTS['filename'] == 'lab4') {
+        if ($PATH_PARTS['filename'] == 'lab4 Home') {
             print ' class="activePage" ';
         }
         print '><a href="index.php">Home Lab4</a></li>';
@@ -17,22 +17,10 @@
         print '><a href="../sitemap.php">Sitemap</a></li>';
        
         print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery2') {
+        if ($PATH_PARTS['filename'] == 'Form') {
             print ' class="activePage" ';
         }
-        print '><a href="sampleQuery2.php">Sample INSERT</a></li>';
-        
-        print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery3') {
-            print ' class="activePage" ';
-        }
-        print '><a href="sampleQuery3.php">Sample SELECT (with ORDER clause)</a></li>';
-        
-        print '<li ';
-        if ($PATH_PARTS['filename'] == 'sampleQuery4') {
-            print ' class="activePage" ';
-        }
-        print '><a href="sampleQuery4.php">Sample test (fails)</a></li>';
+        print '><a href="form.php">Form</a></li>';
         
         print '<li ';
         if ($PATH_PARTS['filename'] == 'tables') {
